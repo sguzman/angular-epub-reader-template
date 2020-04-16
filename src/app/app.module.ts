c7f66d89e6826a062063b3fe8d77d29ebd3fd5fe
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-16 02:29:22 -0700

    add tabs module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatTabsModule,
     BrowserAnimationsModule
   ],
   providers: [],
